dd9e7cf33c708071f91c7baecb8dfdf980730d65
DESKTOP-7LHIHU9\99445 <994458697@qq.com>
2023-07-13 04:41:18 +0800

    static card height and preview modal

--- src/components/messageItem.js
+++ src/components/messageItem.js
@@ -1,33 +1,57 @@
 import dayjs from 'dayjs';
-import { Card, Avatar, Image, Divider } from "antd";
+import { Card, Avatar, Image, Divider, contentInPopover, Typography, Modal } from "antd";
+import { useState } from 'react';
 const { Meta } = Card
+const { Paragraph } = Typography
 
 const MessageItem = (props) => {
+    const [open, setModalOpen] = useState(false)
+
     const { item: message } = props
+
+    const { senderAvatar } = message
+    const avatar = <Avatar src={senderAvatar} />
+
+    const getRender = (type) => {
+        const render = (content, index) => {
+            const { msgType } = content
+            const { text, imageUrl, imageThumbnailUrl } = content
+            const commonImageProps = {
+                key: imageUrl,
+                src: imageUrl,
+                placeholder: <Image preview={false} src={imageThumbnailUrl} />
+            }
+            const displayContent = {
+                1: text,
+                3: <Image preview={type == 'detail'} {...commonImageProps} />
+            }
+            return displayContent[msgType] || ''
+        }
+        return render
+    }
 
     const { msgContent } = message
 
-
-    const { senderAvatar, senderNick } = message
-    const sender = <Meta
-        avatar={<Avatar src={senderAvatar} />}
-        description={senderNick}
-    />
-
-    const contentRender = (content, index) => {
-        const { msgType } = content
-        const { text, imageUrl, imageThumbnailUrl } = content
-        const displayContent = {
-            1: text,
-            3: <Image height='15vh' key={imageUrl} src={imageUrl} placeholder={<Image preview={false} src={imageThumbnailUrl} />} />
-        }
-        return displayContent[msgType] || ''
+    const { senderNick } = message
+    const previewSenderNick = <Paragraph ellipsis={{ rows: 1, tooltip: true }}>{senderNick}</Paragraph>
+    const previewSender = <Meta avatar={avatar} description={previewSenderNick} />
+    const previewProps = {
+        onClick: () => setModalOpen(true),
+        style: { height: 70 },
+        ellipsis: { rows: 3 }
     }
-    const content = <>{msgContent?.map(contentRender)}</>
+    const previewContent = <Paragraph {...previewProps}>{msgContent?.map(getRender('preview'))}</Paragraph>
+    const preview = <>{previewSender}<Divider />{previewContent}</>
 
-    const itemWrapper = component => <Card className='messageItem'>{component}</Card>
+    const sender = <Meta avatar={avatar} description={senderNick}></Meta>
+    const detailStyle = { height: '60vh', overflowY: 'auto' }
+    const content = <div style={detailStyle}>{msgContent?.map(getRender('detail'))}</div>
+    const detail = <Card bordered={false}>{sender}<Divider />{content}</Card>
+    const modal = <Modal centered open={open} onCancel={e => setModalOpen(false)} footer={null} >{detail}</Modal>
 
-    return itemWrapper(<>{sender}<Divider />{content}</>)
+
+    return <Card className='messageItem'>{modal}{preview}</Card>
+
 }
 
 export default MessageItem
